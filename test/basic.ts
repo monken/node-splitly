4efedfc1f36c6173b03efbcd26ecf7722435e2c4
Moritz Onken <onken@netcubed.de>
2019-12-02 13:58:36 -0800

    tests

---
+++ test/basic.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import 'mocha';
+
+import { createStream } from '../lib';
+
+interface Test {
+  name: string;
+  chunks: Array<string | Buffer>;
+  lines: Array<string>;
+};
+
+describe('splitstream', () => {
+  describe('basic', () => {
+
+    const tests = [{
+      name: 'two lines',
+      chunks: ['foo\n', 'bar\n'],
+      lines: ['foo\n', 'bar\n'],
+    }, {
+      name: 'two lines as Buffers',
+      chunks: ['foo\n', 'bar\n'].map(c => Buffer.from(c)),
+      lines: ['foo\n', 'bar\n'],
+    }, {
+      name: 'no traling new line',
+      chunks: ['foo\nbar\nbar'],
+      lines: ['foo\n', 'bar\n', 'bar'],
+    }, {
+      name: 'no newline in first chunk',
+      chunks: ['foobar', 'foo\nbar'],
+      lines: ['foobarfoo\n', 'bar'],
+    }, {
+      name: 'one large chunk',
+      chunks: Array.from({ length: 1000 }, (v, a) => `line ${a * 2}\nline ${a * 2 + 1}\n`),
+      lines: Array.from({ length: 2000 }, (v, a) => `line ${a}\n`),
+    }, {
+      name: 'many lines',
+      chunks: [Array.from({ length: 2000 }, (v, a) => `line ${a}\n`).join('')],
+      lines: Array.from({ length: 2000 }, (v, a) => `line ${a}\n`),
+    }];
+
+    tests.forEach(({ chunks, lines, name }: Test) =>
+      it(name, (done) => {
+        let processed = 0;
+        const stream = createStream();
+        stream.on('data', (chunk) => {
+          expect(chunk.toString()).to.equal(lines[processed]);
+          processed += 1;
+        });
+        stream.on('end', () => {
+          expect(processed).to.equal(lines.length);
+          done();
+        });
+        chunks.forEach(c => stream.write(c));
+
+        stream.end();
+      })
+    );
+  });
+});
